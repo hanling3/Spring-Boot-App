1d01d4fd1986b4959d6be49b0308c9a9f8a86b43
Hedy Yang <hedyyang1004@gmail.com>
2023-05-05 11:58:30 -0700

    Replace constructor injection with setter injection

--- src/main/java/lec2_component_scanning/rest/CoachController.java
+++ src/main/java/lec2_component_scanning/rest/CoachController.java
@@ -10,9 +10,22 @@ public class CoachController {
     // define a private field for the dependency
     private Coach myCoach;
 
+    /* Spring Injection Types:
+        * Constructor Injection
+        *   -- use it for required dependencies
+        * Setter Injection
+        *   -- use it for optional dependencies
+     */
+
     // define a constructor for dependency injection
+//    @Autowired
+//    public CoachController(Coach c) {
+//        myCoach = c;
+//    }
+
+    // Setter Injection
     @Autowired
-    public CoachController(Coach c) {
+    public void setCoach(Coach c) {
         myCoach = c;
     }
 
