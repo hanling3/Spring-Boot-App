314ef6de844e90b477b430fa1c5c1a6e267ec784
Hedy Yang <hedyyang1004@gmail.com>
2023-05-05 19:02:23 -0700

    Set the bean scope to prototype

--- src/main/java/lec7_bean_scopes/coaches/CricketCoach.java
+++ src/main/java/lec7_bean_scopes/coaches/CricketCoach.java
@@ -1,8 +1,12 @@
 package lec7_bean_scopes.coaches;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE) // new object intsance is created for each injection
 public class CricketCoach implements Coach {
     public CricketCoach() {
         System.out.println("In constructor: " + getClass().getSimpleName());
